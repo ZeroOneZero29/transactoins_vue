e3ee98710834cca47ab22a522483a86177696a83
ZeroOneZero29 <zhigadlov29@gmail.com>
2023-07-02 03:24:12 +0700

    second committ

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="section-container">
+            <h1 class="main_title">Пополнение счета</h1>
+            <div class="subtitle-container">
+                <img src='require("@/assets/img/icon/walet-ligth.png")' alt="walet" class="subtitle_img">
+                <h2 class="subtitle">Выбирете платежную систему:</h2>
+            </div>
+        </div>
 </template>
-
+<script>
+export default {
+    
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap");
+body {
+  font-family: Montserrat;
+  font-style: normal;
+  background-color: #F0FAFE;
+  padding: 0 15px;
 }
 
-nav {
-  padding: 30px;
+.section-container {
+  max-width: 840px;
+  margin: 0 auto;
+  margin-top: clamp(3.75rem, 2.134rem + 6.9vw, 5.625rem);
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.main_title {
+  color: #0F2D97;
+  font-size: clamp(2.25rem, 1.172rem + 4.6vw, 3.5rem);
+  font-weight: 700;
+  margin-bottom: clamp(2.5rem, 1.422rem + 4.6vw, 3.75rem);
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.subtitle-container {
+  display: flex;
+  gap: clamp(0.875rem, 0.336rem + 2.3vw, 1.5rem);
 }
+
+.subtitle_img {
+  -o-object-fit: contain;
+     object-fit: contain;
+}
+
+.subtitle {
+  color: #0F2D97;
+  font-size: clamp(1.5rem, 0.853rem + 2.76vw, 2.25rem);
+  font-weight: 600;
+}/*# sourceMappingURL=main.css.map */
 </style>
---
+++ src/components/Titles.vue
@@ -0,0 +1,21 @@
+<template >
+    <div class="section-container">
+            <h1 class="main_title">Пополнение счета</h1>
+            <div class="subtitle-container">
+                <img src="./img/icon/wallet-light.png" alt="walet" class="subtitle_img">
+                <h2 class="subtitle">Выбирете платежную систему:</h2>
+            </div>
+        </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss">
+    
+</style>
